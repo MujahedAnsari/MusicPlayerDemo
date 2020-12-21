e0299b684650c40d52aa9af43721349e7e73a882
Mujahed Ansari <email2mujahed@gmail.com>
2020-12-21 09:26:32 +0530

    first changes.

--- MusicPlayerDemo/ViewController.swift
+++ MusicPlayerDemo/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        playMusic()
         // Do any additional setup after loading the view.
     }
 
-
+    func playMusic(){
+      
+        let path = Bundle.main.path(forResource: "findPhoneRingtune", ofType : "mp3")!
+        let url = URL(fileURLWithPath : path)
+            do {
+                
+                 player = try AVAudioPlayer(contentsOf: url)
+                try AVAudioSession.sharedInstance().setCategory(.playAndRecord)
+            } catch {
+                print ("There is an issue with this code!")
+            }
+        DispatchQueue.main.async {
+            //self.player.delegate = self
+            self.player.numberOfLoops = 5
+          //  self.player.prepareToPlay()
+            self.player.play()
+        }
+    }
 }
 
+extension ViewController: AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        print(flag)
+    }
+    
+    func audioPlayerBeginInterruption(_ player: AVAudioPlayer) {
+        print("audioPlayerBeginInterruption")
+    }
+    
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        print("audioPlayerDecodeErrorDidOccur")
+    }
+    
+    func audioPlayerEndInterruption(_ player: AVAudioPlayer, withOptions flags: Int) {
+        print("audioPlayerEndInterruption")
+    }
+    
+}
